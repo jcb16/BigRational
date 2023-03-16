73bca118ccd179c864f2c6becb0c562c7718bae6
Jakub Kolber <115322984+jcb16@users.noreply.github.com>
2023-03-16 18:01:11 +0100

    Dodaj pliki projektów.

---
+++ BigRationalLib/BigRational.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+
+namespace BigRationalLib
+{
+    public readonly struct BigRational
+    {
+        //reprezentacja wewnętrzna
+        public BigInteger Numerator { get; init; }
+        public BigInteger Denominator { get; init; }
+        //stałe         //konstruktory
+        public BigRational(BigInteger numerator, BigInteger denominator) //tu jest wypasiona logika
+        {
+            Numerator = numerator;
+            Denominator = denominator;             //normalizacja znaku
+            if (Denominator < 0)
+            {
+                Numerator = Numerator * (-1);
+                Denominator = Denominator * (-1);
+            }
+            if (Denominator < 0 && Numerator < 0)
+            {
+                Numerator = Numerator * (-1);
+                Denominator = Denominator * (-1);
+            }             //skracanie
+            BigInteger a = Numerator;
+            BigInteger b = Denominator;
+            if (Numerator > Denominator)
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+            }
+            Numerator = Numerator / a;
+            Denominator = Denominator / a;
+        }
+        public BigRational() : this(0, 1)//a to wywołanie konstruktora 
+        {
+        }
+        //ToString
+        //override ToString
+        //public static BigRational Parse(string napis)
+        //"8/3" -> 8/3
+        //"32/-12" -> -8/3         
+
+
+        //równość         
+
+
+        //arytmetyka         
+
+
+        //relacje         
+
+
+        //konwersje, parsowanie     }
+    }
+}
+
---
+++ BigRationalTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+//using BigRationalLib;
+
+//namespace BigRationalTests
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//        }
+//    }
+//}
+using BigRationalLib; namespace BigRationalTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, 1, 2)]
+        [DataRow(3, 2, 3, 2)]
+        [DataRow(1, -2, -1, 2)] //normalizacja znaku
+        [DataRow(-1, -2, 1, 2)] //normalizacja znaku
+        [DataRow(-1, 2, -1, 2)] //normalizacja znaku
+        [DataRow(2, 4, 1, 2)]   //skracanie
+        [DataRow(12, 32, 3, 8)] //skracanie
+        [DataRow(32, 12, 8, 3)] //skracanie        
+        public void Test_Constructor_2args_OK(int a, int b, int x, int y)
+        {
+            BigRational u = new BigRational(a, b); Assert.AreEqual(x, u.Numerator);
+            Assert.AreEqual(y, u.Denominator);
+        }
+    }
+}
